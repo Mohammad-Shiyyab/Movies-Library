ef9b8de071bdfb8de2ae2efba62d37b2631e5f5d
Mohammad-Shiyab <mohammadshiyab1996@gmail.com>
2023-03-24 23:55:50 +0300

    finallab12

--- server.js
+++ server.js
@@ -1,35 +1,74 @@
 const express = require ('express')
 const cors = require('cors')
+const axios = require('axios')
 const app = express()
 app.use(cors())
+require('dotenv').config()
 
-app.get('/', (req, res) => {
+
+app.get('/favorite', getFavorite)
+app.get('/trending', getTrending)
+app.get('/search', searchHandler)
+app.get('/movie', movieHandler)
+app.get('/rate', rateHandler)
+
+
+app.get('/', (req, res) => {                    
     const myData = require('./data.json')
     const resData = new MyData(myData)
     res.json(resData)
 })
 
-app.get('/favorite', (req, res) => {
+function getFavorite (req, res)  {
     res.send('Welcome to Favorite Page')
-})
+}
 
-app.get('/trending ',(req, res, next) => {
-    const key = process.env.API_KEY
-    const url = `https://api.themoviedb.org/3/trending/all/week?api_key=${key}&language=en-US`
+function getTrending(req, res, next)  {
+    const url = `https://api.themoviedb.org/3/trending/all/week?api_key=bb696566aeb1c17e12b8f63e878cbfbf&language=en-US`
     const params = {
         page: req.query.page
     }
-})
+    axios.get(url, { params }).then(mohammad => {
+        mohammad.data.results = mohammad.data.results.map(asa => new Movie(asa))
+        res.json(mohammad.data)
+    }).catch(err => {
+        next(err)
+    })
+}
 
-app.get('/search' ,(req, res, next) =>{
-    const key = process.env.API_KEY
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US`
+
+function searchHandler(req,res){
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=bb696566aeb1c17e12b8f63e878cbfbf&language=en-US&query=The&page=2`
     const params = {
-        query: req.query.query,
         page: req.query.page
     }
-})
+    axios.get(url, { params }).then(mohammad => {
+        mohammad.data.results = mohammad.data.results.map(asa => new Movie(asa))
+        res.json(mohammad.data)
+    }).catch(err => {
+        next(err)
+    })
+}
 
+function movieHandler(req,res,){
+    const url=`https://api.themoviedb.org/3/movie/popular?api_key=bb696566aeb1c17e12b8f63e878cbfbf&language=en-US&page=1`
+    const params={
+        page: req.query.page
+    }
+    axios.get(url, {params}).then(resp => {
+        res.json(resp.data)
+    })
+}
+
+function rateHandler(req,res,){
+    const url=`https://api.themoviedb.org/3/movie/top_rated?api_key=bb696566aeb1c17e12b8f63e878cbfbf&language=en-US&page=1`
+    const params={
+        page: req.query.page
+    }
+    axios.get(url, {params}).then(resp => {
+        res.json(resp.data)
+    })
+}
 app.use((req, res, next) => {
     res.status(404)
     res.json({
@@ -47,10 +86,16 @@ app.use((err, req, res, next) => {
     })
 })
 
-function MyData({id,title,release_date, poster_path, overview }) {
-    this.id=id;
-    this.release_date=release-date;
+function MyData({ title, poster_path, overview }) {
     this.title = title;
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
+
+function Movie({ id, title, release_date, poster_path, overview }) {
+    this.id = id
+    this.title = title;
+    this.release_date = release_date;
     this.poster_path = poster_path;
     this.overview = overview;
 }
